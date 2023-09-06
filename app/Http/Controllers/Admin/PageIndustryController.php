385b8120d919fde2a1367e265a7299b002ad090b
Ashvin Solanki <ashvinniten0002@gmail.com>
2023-09-06 16:22:44 +0530

    Added View For IndustryPage

--- app/Http/Controllers/Admin/PageIndustryController.php
+++ app/Http/Controllers/Admin/PageIndustryController.php
@@ -9,6 +9,22 @@ use Illuminate\Support\Facades\DB;
 
 class PageIndustryController extends Controller
 {
+    public function index()
+    {   
+        $sliders = DB::table('sliders')->get();
+    	$page_home = DB::table('page_home_items')->where('id',1)->first();
+    	$why_choose_items = DB::table('why_choose_items')->get();
+    	$services = DB::table('services')->get();
+    	$companies = DB::table('companies')->get();
+    	$testimonials = DB::table('testimonials')->get();
+    	$projects = DB::table('projects')->get();
+    	$team_members = DB::table('team_members')->get();
+    	$blogs = DB::table('blogs')->get();
+		$case_studies = DB::table('case_studies')->get();
+        $page_home = PageHomeItem::where('id',1)->first();
+        return view('pages.industry', compact('sliders','page_home','why_choose_items','services', 'testimonials','projects','team_members','blogs', 'case_studies','companies'));
+    }
+
     public function edit()
     {
         $page_home = PageHomeItem::where('id',1)->first();
